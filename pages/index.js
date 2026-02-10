ffe9fb03ba1aa1fe71724e6135eee455ee266580
Gabriel Giulian <40202382+Harofry@users.noreply.github.com>
2026-02-10 11:25:11 +0000

    Main mensage modified

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
 function Home () {
-    return <h1>Sir Gabriel de Itaú</h1>
+    return <h1>
+        
+        OBSERVAÇÃO: ESSA É UMA MENSAGEM OCULTA
+        Caso você esteja lendo isso signifca que você é o melhor primo do mundo!
+        
+        Sua recompensa por isso é:
+        1- Uma ida ao cinema com tudo pago 🎥🍿
+        2- Direito a ganhar um jogo (ou ganhar cosméticos de um) 🎮👾
+        3- Ingressos por dois dias da GGCON 
+
+        Resgate com responsabilidade 😎🤝
+        
+    </h1>
 }
  
 export default Home;
