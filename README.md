6abefc949b92b17681b7f5bdd738d1a0c50f9e0c
Gabriel Giulian <40202382+Harofry@users.noreply.github.com>
2026-02-13 03:30:32 +0000

    add: scripts and lint:check, lint:fix

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Personal-developmant-
+This repository is used for my own training in development.
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,32 @@
-function Home () {
-  return ( 
+function Home() {
+  return (
     <div>
       <h3>OBSERVAÇÃO: ESSA É UMA MENSAGEM OCULTA</h3>
 
-      <p>Caso você esteja lendo isso significa que você é o melhor primo do mundo!</p>
+      <p>
+        Caso você esteja lendo isso significa que você é o melhor primo do
+        mundo!
+      </p>
       <p>Suas recompensas por isso são:</p>
 
       <ul>
-        <li><strong>1-</strong> Uma ida ao cinema com tudo pago 🎥🍿</li>
-        <li><strong>2-</strong> Direito a ganhar um jogo (ou ganhar cosméticos de um) 🎮👾</li>
-        <li><strong>3-</strong> Ingressos para dois dias da GGCON 🎟️🎪</li>
+        <li>
+          <strong>1-</strong> Uma ida ao cinema com tudo pago 🎥🍿
+        </li>
+        <li>
+          <strong>2-</strong> Direito a ganhar um jogo (ou ganhar cosméticos de
+          um) 🎮👾
+        </li>
+        <li>
+          <strong>3-</strong> Ingressos para dois dias da GGCON 🎟️🎪
+        </li>
       </ul>
 
-      <p><strong>Resgate com responsabilidade 😎🤝</strong></p>
+      <p>
+        <strong>Resgate com responsabilidade 😎🤝</strong>
+      </p>
     </div>
-  ) 
+  );
 }
 
 export default Home;
