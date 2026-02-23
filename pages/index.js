20c76cf5d4196ff33f7e4f9bd2ac49ca99772b81
Gabriel Giulian <40202382+Harofry@users.noreply.github.com>
2026-02-23 02:11:54 +0000

    fixed the text mensage

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,7 @@
 function Home() {
   return (
     <div>
-      <h3>OBSERVAÇÃO: ESSA É UMA MENSAGEM OCULTA</h3>
-
-      <p>
-        Caso você esteja lendo isso significa que você é o melhor primo do
-        mundo!
-      </p>
-      <p>Suas recompensas por isso são:</p>
-
-      <ul>
-        <li>
-          <strong>1-</strong> Uma ida ao cinema com tudo pago 🎥🍿
-        </li>
-        <li>
-          <strong>2-</strong> Direito a ganhar um jogo (ou ganhar cosméticos de
-          um) 🎮👾
-        </li>
-        <li>
-          <strong>3-</strong> Ingressos para dois dias da GGCON 🎟️🎪
-        </li>
-      </ul>
-
-      <p>
-        <strong>Resgate com responsabilidade 😎🤝</strong>
-      </p>
+      <h3>Estou neste exato momento construindo um novo local na internet.</h3>
     </div>
   );
 }
